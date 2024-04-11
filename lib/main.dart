d10c6d8c62c2cc813478e4faaf68b51eb91839c7
MohammamdAmin <mhmdamyjl12@gmail.com>
2024-04-11 14:57:24 +0430

    main.dart file is almost complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,52 @@
-import 'package:flutter/services.dart';
+import 'package:store_application/register_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:store_application/splash_screen.dart';
 import 'package:store_application/home_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'login_screen.dart';
 
 void main() async {
+  await dotenv.load(fileName: ".env");
   await Supabase.initialize(
     url: 'https://wzmdhsigrbbmiiaudjst.supabase.co',
     anonKey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6bWRoc2lncmJibWlpYXVkanN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIzOTg0MDAsImV4cCI6MjAyNzk3NDQwMH0.y9-IYsHoQOwvThoNhRkV_FJ0AdjARLjHmUU-IK85QAs',
   );
   SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-  runApp(MainScreen());
+    [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
+  );
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.blue,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ),
+  );
+  runApp(const MainScreen());
 }
 
 class MainScreen extends StatelessWidget {
-  final supabase = Supabase.instance.client;
-  MainScreen({super.key});
+  const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      locale: const Locale('fa', 'IR'),
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
       debugShowCheckedModeBanner: false,
       title: 'شناگرشو',
-      home: HomeScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/second': (context) => RegisterScreen(),
+        '/third': (context) => const HomeScreen(),
+        '/fourth': (context) => LoginScreen(),
+      },
     );
   }
 }
