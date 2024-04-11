c4bef3a9229de1e109ecb65eaf3757c98dbd3808
MohammamdAmin <mhmdamyjl12@gmail.com>
2024-04-11 14:58:04 +0430

    add option for signup and signin for users in app

---
+++ lib/login_screen.dart
@@ -0,0 +1,150 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:store_application/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController textEditingPhoneController =
+      TextEditingController();
+
+  final TextEditingController textEditingPasswordController =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final widthMobileSize = MediaQuery.of(context).size.width;
+    final heightMobileSize = MediaQuery.of(context).size.height;
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 235, 235, 235),
+        body: Stack(
+          children: <Widget>[
+            Container(
+              width: widthMobileSize,
+              height: heightMobileSize / 2,
+              color: const Color.fromARGB(255, 29, 29, 29),
+              child: 'صفحه ورود'.textSyleToText(Colors.white, 45),
+            ),
+            Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                ),
+                width: 500,
+                height: 500,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Align(
+                            alignment: Alignment.topLeft,
+                            child:
+                                'شماره تلفن'.textSyleToText(Colors.black, 15)),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextField(
+                            controller: textEditingPhoneController,
+                            keyboardType: TextInputType.emailAddress,
+                            autofocus: true,
+                            decoration: InputDecoration(
+                              prefixIcon: const Icon(Icons.phone),
+                              labelText: 'Please enter your phone number',
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                  color: Colors.grey,
+                                ),
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                  color: Colors.grey,
+                                ),
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: 'رمز عبور'.textSyleToText(Colors.black, 15),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextField(
+                            controller: textEditingPasswordController,
+                            keyboardType: TextInputType.visiblePassword,
+                            autofocus: true,
+                            decoration: InputDecoration(
+                              prefixIcon: const Icon(Icons.lock),
+                              labelText: 'Please enter your password',
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                  color: Colors.grey,
+                                ),
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                  color: Colors.grey,
+                                ),
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              final supabase = Supabase.instance.client;
+                              await supabase.auth
+                                  .signInWithPassword(password: '123',phone:null);
+                              Navigator.pushNamed(context, '/third');
+                            },
+                            style: const ButtonStyle(
+                              backgroundColor: MaterialStatePropertyAll(
+                                Color.fromARGB(255, 29, 29, 29),
+                              ),
+                            ),
+                            child: 'ورود'.textSyleToText(Colors.white, 20),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        'اگر حساب کاربری ندارید'.textSyleToText(Colors.black, 20),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/second');
+                          },
+                          child: 'ثبت نام شوید'.textSyleToText(Colors.black, 20)
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
