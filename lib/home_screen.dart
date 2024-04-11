09dbb438a843a94117d21b9cae229c093a13156c
MohammamdAmin <mhmdamyjl12@gmail.com>
2024-04-12 00:23:01 +0430

    home_screen.dart file created and add some feature

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:store_application/utils/page_view.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,37 +10,101 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final supabase = Supabase.instance.client;
+  int newCurrentIndexClicked = 0;
+  List<Widget> showScreen = [];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          backgroundColor: Colors.amber,
+        ),
+        floatingActionButton: const FloatingActionButton(
+          backgroundColor: Colors.amber,
+          shape: CircleBorder(
+            side: BorderSide.none,
+          ),
+          onPressed: null,
+        ),
+        floatingActionButtonLocation:
+            FloatingActionButtonLocation.miniCenterDocked,
         bottomNavigationBar: BottomNavigationBar(
-          onTap: null,
-          elevation: null,
-          backgroundColor: null,
+          selectedLabelStyle: const TextStyle(
+            color: Colors.black,
+            fontFamily: 'B Mitra',
+            fontWeight: FontWeight.bold,
+            fontSize: 17,
+          ),
+          unselectedLabelStyle: const TextStyle(
+            color: Colors.black,
+            fontFamily: 'B Mitra',
+            fontWeight: FontWeight.bold,
+            fontSize: 17,
+          ),
+          type: BottomNavigationBarType.fixed,
+          currentIndex: newCurrentIndexClicked,
+          onTap: (int index) async {
+            setState(() {
+              newCurrentIndexClicked = index;
+            });
+          },
+          elevation: 9.0,
+          backgroundColor: Colors.amber,
+          fixedColor: Colors.white,
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
+              icon: Icon(Icons.shopping_cart),
+              activeIcon: Icon(Icons.shopping_cart),
+              label: 'سبدخرید',
+              tooltip: 'سبدخرید',
             ),
             BottomNavigationBarItem(
               icon: Icon(Icons.person),
+              activeIcon: Icon(Icons.person),
+              label: 'حساب کاربری',
+              tooltip: 'حساب کاربری',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.add),
+              icon: Icon(Icons.category),
+              activeIcon: Icon(Icons.category),
+              label: 'دسته بندی',
+              tooltip: 'دسته بندی',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              activeIcon: Icon(Icons.home),
+              label: 'خانه',
+              tooltip: 'خانه',
             ),
           ],
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ShowPageView(),
-          ],
+        body: FutureBuilder(
+          future: supabase.from('swimming glasses').select().explain(),
+          builder: (context, snapshot) {
+            print(snapshot.data);
+            if(snapshot.hasData){
+              return Text('salam');
+            }else{
+              return Text('sdf');
+            }
+          },
         ),
       ),
     );
   }
 }
+// Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             //ShowPageView(),
+//             // showScreen.elementAt(newCurrentIndexClicked),
+//             GridView.count(
+//               crossAxisCount: 2,
+//               children: <Widget>[
+                
+//               ],
+//             ),
+//           ],
+//         ),
